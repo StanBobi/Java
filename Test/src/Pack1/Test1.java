56749dd24db1bb9aa3dabcacce49e5905f8c7c0d
STAN Cosmin-Bogdan <61937705+StanBobi@users.noreply.github.com>
2022-01-03 18:16:31 +0200

    Add files via upload

---
+++ Test/src/Pack1/Test1.java
@@ -0,0 +1,32 @@
+package Pack1;
+
+import java.applet.*;
+import java.awt.*;
+
+/*
+public class Test1 extends Applet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+	}
+
+}
+*/
+public class Test1 extends Applet
+{
+	
+	String s1, s2, s3;
+	
+	public void init()
+	{
+		if((s1 == getParameter("arg1"))) s1 = "Nu am gasit arg1 in html";
+		
+	}
+	public void paint(Graphics g)
+	{
+		g.drawString("s1 = " + s1, 20, 20);
+	}
+}
